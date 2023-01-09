296b9034fb24fd678ab2fe7ceebd4319b4b549b7
Wes Bos <wesbos@gmail.com>
2023-01-09 15:23:00 -0500

    create files

---
+++ rename.js
@@ -0,0 +1,8 @@
+export function someFunction() {
+  const name = 'Wes Bos';
+  console.log(name);
+  const age = 100;
+  console.log(age);
+  `I am a .js file`;
+  return `I am ${name} and I am ${age} years old.`;
+}
